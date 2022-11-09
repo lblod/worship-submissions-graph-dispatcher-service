6cfd82942fdf007a804659418cd0a4956e9008d6
Achraf Atauil <achrafatauil@hotmail.com>
2022-11-09 13:16:59 +0100

    add sendErrorAlert query

--- util/queries.js
+++ util/queries.js
@@ -4,6 +4,8 @@ import exportConfig from "../export-config";
 import { parseResult } from './utils';
 import { ORG_GRAPH_BASE, ORG_GRAPH_SUFFIX, DISPATCH_SOURCE_GRAPH } from '../config';
 
+const CREATOR = 'http://lblod.data.gift/services/worship-submissions-graph-dispatcher-service';
+
 export async function getTypesForSubject(subject) {
   const configuredTypes = exportConfig.map(c => sparqlEscapeUri(c.type)).join('\n');
 
@@ -81,5 +83,34 @@ export async function copySubjectDataToDestinators(subject, destinators) {
        }
     `;
     await update(queryStr);
+  }
+}
+
+export async function sendErrorAlert({message, detail, reference}) {
+  if (!message)
+    throw 'Error needs a message describing what went wrong.';
+  const id = uuid();
+  const uri = `http://data.lblod.info/errors/${id}`;
+  const q = `
+      PREFIX mu:   <http://mu.semte.ch/vocabularies/core/>
+      PREFIX oslc: <http://open-services.net/ns/core#>
+      PREFIX dct:  <http://purl.org/dc/terms/>
+      INSERT DATA {
+        GRAPH <http://mu.semte.ch/graphs/error> {
+            ${sparqlEscapeUri(uri)} a oslc:Error ;
+                    mu:uuid ${sparqlEscapeString(id)} ;
+                    dct:subject ${sparqlEscapeString('Dispatch worship submissions')} ;
+                    oslc:message ${sparqlEscapeString(message)} ;
+                    dct:created ${sparqlEscapeDateTime(new Date().toISOString())} ;
+                    dct:creator ${sparqlEscapeUri(CREATOR)} .
+            ${reference ? `${sparqlEscapeUri(uri)} dct:references ${sparqlEscapeUri(reference)} .` : ''}
+            ${detail ? `${sparqlEscapeUri(uri)} oslc:largePreview ${sparqlEscapeString(detail)} .` : ''}
+        }
+      }
+  `;
+  try {
+    await update(q);
+  } catch (e) {
+    console.error(`[WARN] Something went wrong while trying to store an error.\nMessage: ${e}\nQuery: ${q}`);
   }
 }
