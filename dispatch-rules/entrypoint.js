3d5939ae88228d4a80287f9205dfa766df16b2d9
felix <ruiz.felixruiz@gmail.com>
2022-12-28 08:43:47 +0100

    throwing jaarrekening rules around

--- dispatch-rules/entrypoint.js
+++ dispatch-rules/entrypoint.js
@@ -1,6 +1,7 @@
 import notulenRules from './notulen';
 import schorschingsbesluitRules from './schorsingsbesluit';
 import besluitHandhavenRules from './besluit-handhaven';
+import jaarrekeningrules from './jaarrekening';
 
 import { sparqlEscapeUri } from "mu";
 import { toezichthoudendeQuerySnippet, repOrgQuerySnippet } from './query-snippets';
@@ -11,91 +12,16 @@ import { toezichthoudendeQuerySnippet, repOrgQuerySnippet } from './query-snippe
  * It tries to translate this file: https://docs.google.com/spreadsheets/d/1NnZHqaFnNToE-aZMiyDI1QIPhHP5EG8i39KGFhTazlg/edit?usp=sharing
  */
 
-const rules = [ ...notulenRules, ...schorschingsbesluitRules, ...besluitHandhavenRules ];
+const rules = [
+  ...notulenRules,
+  ...schorschingsbesluitRules,
+  ...besluitHandhavenRules,
+  ...jaarrekeningrules
+];
 
 // Excel: Rules number: 9, 11, 13, 15, 17, 19, 21, 23, 25, 27
 // TODO: we need an extension in loket to go 'down the tree', a PO can have multiple EB, CB.
 // This is for a next story
-
-
-/* Excel: Rules number: 63
-* Testing:
-*--------------------------
-* SENDER: <http://data.lblod.info/id/besturenVanDeEredienst/d52de436e194111289248db2d06e99ac> Kerkfabriek O.-L.-Vrouw van Deinze
-* CB: <http://data.lblod.info/id/centraleBesturenVanDeEredienst/7f5475cfb202d12f54779f046441c9e1> CKB Deinze
-**/
-let rule = {
-  documentType: 'https://data.vlaanderen.be/id/concept/BesluitType/e44c535d-4339-4d15-bdbf-d4be6046de2c', //Jaarrekening (JR) - EB met CB
-  matchSentByEenheidClass: eenheidClass => eenheidClass == 'http://data.vlaanderen.be/id/concept/BestuurseenheidClassificatieCode/66ec74fd-8cfc-4e16-99c6-350b35012e86', //EB
-  destinationInfoQuery: ( sender ) => {
-    return `
-      PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
-      PREFIX org: <http://www.w3.org/ns/org#>
-
-      SELECT DISTINCT ?bestuurseenheid ?uuid WHERE {
-        BIND(${sparqlEscapeUri(sender)} as ?sender)
-        {
-          ?bestuurseenheid org:hasSubOrganization ?sender;
-            <http://data.vlaanderen.be/ns/besluit#classificatie> <http://data.vlaanderen.be/id/concept/BestuurseenheidClassificatieCode/f9cac08a-13c1-49da-9bcb-f650b0604054>;
-            mu:uuid ?uuid.
-        } UNION {
-          VALUES ?bestuurseenheid {
-            ${sparqlEscapeUri(sender)}
-          }
-          ?bestuurseenheid mu:uuid ?uuid
-        }
-      }
-    `;
-  }
-};
-rules.push(rule);
-
-
-/* Excel: Rules number: 64, 65, 66
-* Testing:
-*--------------------------
-* -SENDER-: <http://data.lblod.info/id/besturenVanDeEredienst/44329be9ac7054b39adbc583b6203ba2> Protestantse Kerk Christengemeente Emmanuel van Haacht
-* GEMEENTE: <http://data.lblod.info/id/bestuurseenheden/2ad0d123f4a81787572342c394a1917b81752f42d802d1e013941f56b53bdd2a> Haacht
-* PG: <http://data.lblod.info/id/bestuurseenheden/141d9d6b-54af-4d17-b313-8d1c30bc3f5b> ABB
-**/
-rule = {
-  documentType: 'https://data.vlaanderen.be/id/concept/BesluitType/e44c535d-4339-4d15-bdbf-d4be6046de2c', //Jaarrekening (JR) - EB zonder CB
-  matchSentByEenheidClass: eenheidClass => eenheidClass == 'http://data.vlaanderen.be/id/concept/BestuurseenheidClassificatieCode/66ec74fd-8cfc-4e16-99c6-350b35012e86', //EB
-  destinationInfoQuery: ( sender ) => {
-    return `
-      PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
-      PREFIX org: <http://www.w3.org/ns/org#>
-
-      SELECT DISTINCT ?bestuurseenheid ?uuid WHERE {
-        BIND(${sparqlEscapeUri(sender)} as ?sender)
-        {
-          ${toezichthoudendeQuerySnippet()}
-          FILTER NOT EXISTS {
-            ?cb <http://www.w3.org/ns/org#hasSubOrganization> ?sender;
-              <http://data.vlaanderen.be/ns/besluit#classificatie>
-                <http://data.vlaanderen.be/id/concept/BestuurseenheidClassificatieCode/f9cac08a-13c1-49da-9bcb-f650b0604054>
-          }
-        } UNION {
-            VALUES ?bestuurseenheid {
-              <http://data.lblod.info/id/bestuurseenheden/141d9d6b-54af-4d17-b313-8d1c30bc3f5b>
-            }
-            ?bestuurseenheid mu:uuid ?uuid
-
-            FILTER NOT EXISTS {
-              ?cb <http://www.w3.org/ns/org#hasSubOrganization> ?sender;
-                <http://data.vlaanderen.be/ns/besluit#classificatie> <http://data.vlaanderen.be/id/concept/BestuurseenheidClassificatieCode/f9cac08a-13c1-49da-9bcb-f650b0604054>
-          } UNION {
-            VALUES ?bestuurseenheid {
-              ${sparqlEscapeUri(sender)}
-            }
-            ?bestuurseenheid mu:uuid ?uuid
-          }
-        }
-      }
-    `;
-  }
-};
-rules.push(rule);
 
 /* Excel: Rules number: 67, 68, 69
 * Testing:
@@ -104,7 +30,7 @@ rules.push(rule);
 * GEMEENTE: <http://data.lblod.info/id/bestuurseenheden/104f32d7fb8d4b8b61b71717301656f136fe046eabaf126fb3325896b5c2d625> Tongeren
 * PG: <http://data.lblod.info/id/bestuurseenheden/141d9d6b-54af-4d17-b313-8d1c30bc3f5b> ABB
 **/
-rule = {
+let rule = {
   documentType: 'https://data.vlaanderen.be/id/concept/BesluitDocumentType/672bf096-dccd-40af-ab60-bd7de15cc461', // Jaarrekening (JR + Toelagenoverzicht)
   matchSentByEenheidClass: eenheidClass => eenheidClass == 'http://data.vlaanderen.be/id/concept/BestuurseenheidClassificatieCode/f9cac08a-13c1-49da-9bcb-f650b0604054', // CB
   destinationInfoQuery: ( sender ) => {
