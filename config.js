843f07e8ca313f941a0f5da12379eaa1fe5f85cf
felix <ruiz.felixruiz@gmail.com>
2023-03-02 10:41:19 +0100

    Added env variables

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
 export const ORG_GRAPH_BASE = process.env.ORG_GRAPH_BASE || 'http://mu.semte.ch/graphs/organizations';
 export const ORG_GRAPH_SUFFIX = process.env.ORG_GRAPH_SUFFIX || 'ABB_databankErediensten_LB_CompEnts_gebruiker';
 export const DISPATCH_SOURCE_GRAPH = process.env.DISPATCH_SOURCE_GRAPH || 'http://mu.semte.ch/graphs/temp/for-dispatch';
+export const HEALING_CRON = process.env.HEALING_CRON || '00 07 * * 06'; //Weekly on saturday
+export const ENABLE_HEALING = process.env.ENABLE_HEALING == "true";
+
