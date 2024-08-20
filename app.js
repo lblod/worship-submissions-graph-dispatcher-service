bfff277a909130a7b457462c6ddeba4964d987e7
benjay10 <ben.behaeghe@redpencil.io>
2024-08-20 21:46:26 +0200

    Cleanup

--- app.js
+++ app.js
@@ -237,90 +237,87 @@ async function dispatch(submission) {
       destinators = destinators.concat(currDestinators);
     }
 
-    //for(const rule of applicableRules) {
-      //const destinators = await getDestinators(submissionInfo, rule);
-      let relatedSubjects = [ submissionInfo.submission ];
+    let relatedSubjects = [ submissionInfo.submission ];
 
-      for (const config of exportConfig) {
-        const subjects = await getRelatedSubjectsForSubmission(
-          submissionInfo.submission,
-          config.type,
-          config.pathToSubmission
-        );
+    for (const config of exportConfig) {
+      const subjects = await getRelatedSubjectsForSubmission(
+        submissionInfo.submission,
+        config.type,
+        config.pathToSubmission
+      );
 
-        relatedSubjects = [ ...relatedSubjects, ...subjects ];
-      }
-      relatedSubjects = [ ...(new Set(relatedSubjects)) ];
+      relatedSubjects = [ ...relatedSubjects, ...subjects ];
+    }
+    relatedSubjects = [ ...(new Set(relatedSubjects)) ];
 
-      // Scalar product of related subjects and graphs they should be in
-      const allSubjectsAndGraphs = relatedSubjects.reduce((acc, curr) => {
-        destinators.forEach((d) => {
-          acc.push({
-            subject: curr,
-            graph: ORG_GRAPH_BASE + '/' + d.uuid + '/' + ORG_GRAPH_SUFFIX
-          });
+    // Scalar product of related subjects and graphs they should be in
+    const allSubjectsAndGraphs = relatedSubjects.reduce((acc, curr) => {
+      destinators.forEach((d) => {
+        acc.push({
+          subject: curr,
+          graph: ORG_GRAPH_BASE + '/' + d.uuid + '/' + ORG_GRAPH_SUFFIX
         });
-        return acc;
-      }, []);
-
-      // Count number of triples per subject
-      let counts = await getGraphsAndCountForSubjects(relatedSubjects, [DISPATCH_SOURCE_GRAPH, DISPATCH_FILES_GRAPH]);
-      // Deduplicate the counts
-      // In certain scenario's, the physical file triples are not completely
-      // dispatched correctly between the temp/for-dispatch and the
-      // temp/original-physical-files-data graphs. Take the max number of
-      // triples that can be found in order to become consistent.
-      counts = counts.reduce((acc, curr) => {
-        const alreadySeen = acc.find((e) => e.subject === curr.subject);
-        if (alreadySeen) {
-          alreadySeen.count = Math.max(alreadySeen.count, curr.count);
-        } else {
-          acc.push(curr);
-        }
-        return acc;
-      }, []);
-      allSubjectsAndGraphs.forEach((e) => {
-        e.count = counts.find((f) => f.subject === e. subject)?.count;
       });
+      return acc;
+    }, []);
 
-      // List of subjects and the graph they are in
-      const subjectsAndGraphs = await getGraphsAndCountForSubjects(relatedSubjects);
-
-      // Find subjects that no longer have a correct destinator by calculating a difference
-      const removeSubjectsPerGraph = [];
-      for (const currSub of subjectsAndGraphs) {
-        const found = allSubjectsAndGraphs.find((e) =>
-          e.subject === currSub.subject &&
-          e.graph === currSub.graph);
-        if (!found)
-          removeSubjectsPerGraph.push(currSub);
+    // Count number of triples per subject
+    let counts = await getGraphsAndCountForSubjects(relatedSubjects, [DISPATCH_SOURCE_GRAPH, DISPATCH_FILES_GRAPH]);
+    // Deduplicate the counts
+    // In certain scenario's, the physical file triples are not completely
+    // dispatched correctly between the temp/for-dispatch and the
+    // temp/original-physical-files-data graphs. Take the max number of
+    // triples that can be found in order to become consistent.
+    counts = counts.reduce((acc, curr) => {
+      const alreadySeen = acc.find((e) => e.subject === curr.subject);
+      if (alreadySeen) {
+        alreadySeen.count = Math.max(alreadySeen.count, curr.count);
+      } else {
+        acc.push(curr);
       }
+      return acc;
+    }, []);
+    allSubjectsAndGraphs.forEach((e) => {
+      e.count = counts.find((f) => f.subject === e. subject)?.count;
+    });
 
-      for (const { subject, graph } of removeSubjectsPerGraph) {
-        await removeSubjectFromGraph(subject, graph);
-      }
+    // List of subjects and the graph they are in
+    const subjectsAndGraphs = await getGraphsAndCountForSubjects(relatedSubjects);
 
-      // Difference between the two lists, only ones remaining are the missing or incorrect ones
-      const missingSubjectsPerGraph = [];
-      for (const allSub of allSubjectsAndGraphs) {
-        const found = subjectsAndGraphs.find((e) => 
-          e.subject === allSub.subject &&
-          e.graph === allSub.graph);
-        if (found) {
-          if (found.count > allSub.count) {
-            allSub.toRemoveFirst = true;
-            missingSubjectsPerGraph.push(allSub);
-          } else if (found.count < allSub.count) {
-            missingSubjectsPerGraph.push(allSub);
-            // No else. If counts are equal, nothing needs to be done.
-          }
-        } else {
+    // Find subjects that no longer have a correct destinator by calculating a difference
+    const removeSubjectsPerGraph = [];
+    for (const currSub of subjectsAndGraphs) {
+      const found = allSubjectsAndGraphs.find((e) =>
+        e.subject === currSub.subject &&
+        e.graph === currSub.graph);
+      if (!found)
+        removeSubjectsPerGraph.push(currSub);
+    }
+
+    for (const { subject, graph } of removeSubjectsPerGraph) {
+      await removeSubjectFromGraph(subject, graph);
+    }
+
+    // Difference between the two lists, only ones remaining are the missing or incorrect ones
+    const missingSubjectsPerGraph = [];
+    for (const allSub of allSubjectsAndGraphs) {
+      const found = subjectsAndGraphs.find((e) => 
+        e.subject === allSub.subject &&
+        e.graph === allSub.graph);
+      if (found) {
+        if (found.count > allSub.count) {
+          allSub.toRemoveFirst = true;
           missingSubjectsPerGraph.push(allSub);
+        } else if (found.count < allSub.count) {
+          missingSubjectsPerGraph.push(allSub);
+          // No else. If counts are equal, nothing needs to be done.
         }
+      } else {
+        missingSubjectsPerGraph.push(allSub);
       }
+    }
 
-      for (const { subject, graph, toRemoveFirst } of missingSubjectsPerGraph)
-        await copySubjectDataToGraph(subject, graph, toRemoveFirst);
-    //}
+    for (const { subject, graph, toRemoveFirst } of missingSubjectsPerGraph)
+      await copySubjectDataToGraph(subject, graph, toRemoveFirst);
   }
 }
