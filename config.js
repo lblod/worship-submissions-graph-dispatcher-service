5e627609a9c22dc9b0b167b3725b8a279f9174c1
felix <ruiz.felixruiz@gmail.com>
2025-06-05 11:20:57 +0200

    Added an env variable.

--- config.js
+++ config.js
@@ -5,3 +5,4 @@ export const DISPATCH_FILES_GRAPH = process.env.DISPATCH_FILES_GRAPH || 'http://
 export const HEALING_CRON = process.env.HEALING_CRON || '00 07 * * 06'; //Weekly on saturday
 export const ENABLE_HEALING = process.env.ENABLE_HEALING == "true";
 export const NUMBER_OF_HEALING_QUEUES = parseInt(process.env.NUMBER_OF_HEALING_QUEUES) || 1;
+export const ABB_UUID = process.env.ABB_UUID || "141d9d6b-54af-4d17-b313-8d1c30bc3f5b";
